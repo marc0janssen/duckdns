4c06d95d5b12b35974709134868c77fea3fe098a
Marco <marco@mjanssen.nl>
2023-12-27 20:59:19 +0100

    fixed

--- app/duck.sh
+++ app/duck.sh
@@ -4,4 +4,4 @@
 
 echo ${TOKEN}
 
-echo url="https://www.duckdns.org/update?domains=bulky.duckdns.org&token=${TOKEN}f&ip=" | curl -k -o /logging/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=bulky.duckdns.org&token=${TOKEN}&ip=" | curl -k -o /logging/duckdns/duck.log -K -
