39d2c07a62e9ccf69c9745bb9f67e9617a289187
Marco <marco@mjanssen.nl>
2024-01-06 11:16:19 +0100

    added DOMAIN property

--- app/duck.sh
+++ app/duck.sh
@@ -3,5 +3,6 @@
 . /config/duckdns.ini
 
 echo ${TOKEN}
+echo $(DOMAIN)
 
-echo url="https://www.duckdns.org/update?domains=bulky.duckdns.org&token=${TOKEN}&ip=" | curl -k -o /logging/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=" | curl -k -o /logging/duckdns/duck.log -K -
