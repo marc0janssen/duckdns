565d8cdc826d69cee03b5a6881dcd8858724bae8
Marco <marco@mjanssen.nl>
2025-03-30 21:29:35 +0200

    typo

--- app/duck.sh
+++ app/duck.sh
@@ -3,6 +3,6 @@
 . /config/duckdns.ini
 
 echo ${TOKEN}
-echo $(DOMAIN)
+echo ${DOMAIN}
 
 echo url="https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=" | curl -k -o /var/log/duck.log -K -
